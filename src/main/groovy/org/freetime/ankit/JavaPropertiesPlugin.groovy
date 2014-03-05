37e922a8c0a8a70290af995fbeaa73f5dbc92ab3
ankit rokde <ankitro@thoughtworks.com>
2014-03-05 14:35:12 +0530

    merging jsons working --ankit

--- src/main/groovy/org/freetime/ankit/JavaPropertiesPlugin.groovy
+++ src/main/groovy/org/freetime/ankit/JavaPropertiesPlugin.groovy
@@ -2,11 +2,18 @@ package org.freetime.ankit
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import groovy.json.JsonSlurper
 
 class JavaPropertiesPlugin implements Plugin<Project> {
     void apply(Project project){
         project.task("generateProperties") << {
             println "Plugin Running!!!"
+            def jsonSlurper = new JsonSlurper()
+            def defaults = jsonSlurper.parseText("{ \"key\": \"value\" }")
+            def override = jsonSlurper.parseText("{ \"key\": \"override\" }")
+            def propertyMap = new HashMap(defaults)
+            propertyMap.putAll(override)
+            println propertyMap
         }
     }
 }
